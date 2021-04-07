6784d8da254c1d45d90353160c2a63ed509456ed
AFPA1701871 <martine.poix@afpa.fr>
2021-04-07 09:42:59 +0200

    php web

---
+++ 04 - Php Web/Ex1 Découpage php/page for.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php
+   for ($i=0; $i < 5; $i++) { 
+       echo "<div>test".$i."</div>";
+   }
+        
+   ?>
+</body>
+</html>
+
+
+<!-- //59011vmfic02.exchange.ad.afpanet/59011-data/grp-15/ -->
