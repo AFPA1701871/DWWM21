ad78e16ca6e2f7e2a436b32bfcb3f1129fe0d460
AFPA1701871 <martine.poix@afpa.fr>
2021-04-07 10:16:29 +0200

    php web

---
+++ 04 - Php Web/Ex2 Plusieurs fichiers/header.php
@@ -0,0 +1,10 @@
+
+<body>
+    <div class="page">
+        <header>
+            <div class="logo">
+                <img src="image/cookie.png" alt="cookie" />
+            </div>
+            <div class="titrePage"><?= $titre ?>!</div>
+            <div class="logo"></div>
+        </header>
---
+++ 04 - Php Web/Ex2 Plusieurs fichiers/index.php
@@ -0,0 +1,8 @@
+<?php
+$titre="TOTO";
+include "head.php";
+include "header.php";
+include "nav.php";
+include "page2.php";
+include "footer.php";
+
