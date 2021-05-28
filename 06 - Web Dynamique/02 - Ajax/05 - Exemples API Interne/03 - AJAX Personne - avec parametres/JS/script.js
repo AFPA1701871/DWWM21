c1b0f3d39a533021c98df5159d981ea6659cd45d
afpa1701871 <martine.poix@afpa.fr>
2021-05-28 08:48:14 +0200

    api interne avec parametres

--- 06 - Web Dynamique/02 - Ajax/05 - Exemples API Interne/03 - AJAX Personne - avec parametres/JS/script.js
+++ 06 - Web Dynamique/02 - Ajax/05 - Exemples API Interne/03 - AJAX Personne - avec parametres/JS/script.js
@@ -1,6 +1,10 @@
 const requ = new XMLHttpRequest();
-requ.open('GET', 'PHP/Model/API/Count.php', true);
-requ.send(null);
+requ.open('POST', 'index.php?codePage=listeAPI', true);
+requ.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//var args = "idPersonne=2";
+ var args = "idPersonne=1&test=2";
+requ.send(args);
+
 
 requ.onreadystatechange = function(event) {
     // XMLHttpRequest.DONE === 4
@@ -10,10 +14,11 @@ requ.onreadystatechange = function(event) {
             var divCount  = document.getElementById("total");
             reponse=JSON.parse(this.responseText);
             console.log(reponse);
-            divCount.innerHTML=reponse.nb;
+           //on traite les éléments de la liste ....
         } else {
             console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
         }
     }
 };
+
 
